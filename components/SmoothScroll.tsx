c898d9976848aa7b8b2deba8a12c2a71bf6efecf
jarvis-invictus <jarvis.invictus.ai@gmail.com>
2026-05-27 01:07:00 +0530

    Fix: Correct lenis import - use @studio-freight/lenis package

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactLenis } from "lenis";
+import { ReactLenis } from "@studio-freight/lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (<ReactLenis root options={{ duration: 1.2, smoothWheel: true }}>{children}</ReactLenis>);
